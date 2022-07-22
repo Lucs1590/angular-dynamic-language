b4ddb5f5cbc6de1250b677405429ede188f52256
Lucas Brito <lucasbsilva29@gmail.com>
2022-07-21 22:26:23 -0300

    feat(app.component.ts): add translate feature on main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { lastValueFrom } from 'rxjs';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1 translate> mainPhrase </h1>
+      <h2 translate> subPhrase </h2>
+      <button translate > buttonPhrase </button>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'angular-dynamic-language';
+
+export class AppComponent implements OnInit {
+
+  constructor(
+    private translate: TranslateService,
+    private apiService: ApiService
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    await this.setLanguage();
+  }
+
+  async setLanguage(): Promise<void> {
+    const ipInfo$ = this.apiService.getIPInfo();
+    const ipInfo = await lastValueFrom(ipInfo$);
+
+    this.translate.setDefaultLang('en');
+    if (ipInfo?.country_code?.toUpperCase() == 'BR') {
+      this.translate.setDefaultLang('pt');
+    };
+  }
+
 }
